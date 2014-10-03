5d400814e1c9ff9b006e19bb241dcc17a7af46c4
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-03 13:49:26 -0500

    Change include to yield to fix breaking syntax

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('layouts.include_header')
-    @include('top-script')
+    @yield('top-script')
 </head>
 <body>
 
-@yield('content')
+@yield('body')
 
 @yield('bottom-script')
 @include('layouts.include_footer')
