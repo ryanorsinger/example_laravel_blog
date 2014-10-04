39bceaeb79410ee1afd2c02de671c86982e71e13
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-04 13:08:18 -0500

    Add resume and portfolio

---
+++ app/views/portfolio.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Portfolio</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<h1>This is my awesome portfolio.</h1>
+<h2>Here, you will see my projects.</h2>
+</body>
+</html>
