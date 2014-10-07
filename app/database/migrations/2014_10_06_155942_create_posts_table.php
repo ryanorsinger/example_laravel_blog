d9c0ceeb0b67d876dfd44afce0f6ed7c56e7d3b0
ryanorsinger <ryan.orsinger@gmail.com>
2014-10-07 09:19:30 -0500

    Remove user_id foreign from posts table (for now)

--- app/database/migrations/2014_10_06_155942_create_posts_table.php
+++ app/database/migrations/2014_10_06_155942_create_posts_table.php
@@ -15,9 +15,6 @@ class CreatePostsTable extends Migration {
 		Schema::create('posts', function($table)
         {
             $table->increments('id');
-            $table->integer('user_id')
-                  ->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
             $table->string('title', 100);
             $table->text('body');
             $table->timestamps();
