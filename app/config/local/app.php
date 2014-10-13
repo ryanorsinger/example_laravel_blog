a222df8cc9875c6aac4ada163923f14c5e99b908
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-13 14:59:42 -0500

    Add debugbar package.

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -16,5 +16,21 @@ return array(
 	'debug' => true,
 	'providers' => array(
 		'Way\Generators\GeneratorsServiceProvider',
-		);
+		'Barryvdh\Debugbar\ServiceProvider',
+		),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Class Aliases
+	|--------------------------------------------------------------------------
+	|
+	| This array of class aliases will be registered when this application
+	| is started. However, feel free to register as many as you wish as
+	| the aliases are "lazy" loaded so they don't hinder performance.
+	|
+	*/
+
+	'aliases' => array(
+		'Debugbar' => 'Barryvdh\Debugbar\Facade',
+	)
 );
